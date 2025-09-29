eeb0d7dc698ff3f330ca1f6cb4efe19877875127
Saini <upiottxat@yahoo.com>
2025-09-29 16:00:02 +0530

    configure lint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "parser": "babel-eslint",
+    "plugins": ["react"],
+    "extends": ["eslint:recommended", "plugin:react/recommended"]
+};
